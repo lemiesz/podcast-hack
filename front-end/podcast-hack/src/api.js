26e8b531bb0c6966f58b3cf35010dfff94beb464
Robert Lemiesz <lemieszrobert@gmail.com>
2021-10-23 19:11:24 -0700

    Adding a basic API contract:

---
+++ front-end/podcast-hack/src/api.js
@@ -0,0 +1,110 @@
+class User {
+  constructor({ name, email, uid, podcasts }) {
+    // name string
+    this.name = name;
+    // string
+    this.email = email;
+    // unique string
+    this.id = uid;
+    // array of refrences to podcasts
+    this.podcasts = podcasts || [];
+  }
+}
+
+class Podcast {
+  constructor({ name, descrtion, fileLocation, id, relatedAds }) {
+    // string
+    this.name = name;
+    // string
+    this.descrtion = descrtion;
+    // unique storage url for this podcast
+    this.fileLocation = fileLocation;
+    // unique id of podcast
+    this.id = id;
+    // a list of related ads with timestamps
+    this.relatedAds = relatedAds;
+  }
+}
+
+function wrapInPromise(obj) {
+  return new Promise((resolve) => {
+    return resolve(obj);
+  });
+}
+
+class Api {
+  login() {
+    return wrapInPromise(
+      User({
+        name: "Noah",
+        email: "noah@noah.com",
+        id: "1",
+      })
+    );
+  }
+
+  logout() {
+    return wrapInPromise(true);
+  }
+
+  /**
+   * This is called when a user attempt to upload a podcast
+   */
+  uploadPodcast({
+    // Buffer of sound file of type File
+    fileRef,
+    // string
+    name,
+    // string
+    description,
+  }) {
+    alert("This has not be implemented yet");
+    return wrapInPromise(
+      new Podcast({
+        name: "fake",
+        descrtion: "this is the description of the podcast",
+        fileLocation: "file.com",
+        id: "123",
+      })
+    );
+  }
+
+  getPodcast({
+    // uniqe id that refresnces this podcast
+    id,
+  }) {
+    return wrapInPromise(
+      new Podcast({
+        name: "fake",
+        descrtion: "this is the description of the podcast",
+        fileLocation: "file.com",
+        id: "123",
+      })
+    );
+  }
+
+  /**
+   * Define on advertisment associated with podcast
+   */
+  defineAdOnPodcast({
+      podcastId,
+      name,
+      timeStartMs,
+      timeEndMs,
+      adFileRef
+  }) {
+      return wrapInPromise(true)
+  }
+
+  /**
+   * Creates a new spliced together sound file containing 
+   * the new ads that we wanted to splice in. 
+   * 
+   * Returns a url refrence of that sound file.
+   */
+  publishPodcast({id}) {
+      return wrapInPromise("https://sound.com");
+  }
+}
+
+export default new Api();
--- front-end/podcast-hack/src/components/UploadPocast/index.js
+++ front-end/podcast-hack/src/components/UploadPocast/index.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
+import api from '../../api';
 
 function UploadPodcast() {
     const [selectedFile, setSelectedFile] = useState();
     const [isFileLoaded, setIsFileLoaded] = useState(false);
 
     const fileUpload = (e) => {
+        // await api.uploadPodcast();
         setSelectedFile(e.target.value);
         setIsFileLoaded(true);
     }
