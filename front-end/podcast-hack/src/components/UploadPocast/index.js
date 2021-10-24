bfe853f42f6ddca8b5f2f3a24fada8d3058645fc
NoahNim <nmedoff123@gmail.com>
2021-10-23 18:30:23 -0700

    file upload functionality onchange

--- front-end/podcast-hack/src/components/UploadPocast/index.js
+++ front-end/podcast-hack/src/components/UploadPocast/index.js
@@ -2,11 +2,16 @@ import React, {useState} from 'react';
 
 function UploadPodcast() {
     const [selectedFile, setSelectedFile] = useState();
-    const [isFileLoaded, setFileLoaded] = useState(false);
+    const [isFileLoaded, setIsFileLoaded] = useState(false);
+
+    const fileUpload = (e) => {
+        setSelectedFile(e.target.value);
+        setIsFileLoaded(true);
+    }
 
     return (
         <div>
-            <input type="file" name="file"></input>
+            <input type="file" name="file" onChange={fileUpload}></input>
             <button>Submit</button>
         </div>
     )
