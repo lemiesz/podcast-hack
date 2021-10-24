0a919272888b7c2a29ace8fce743892c2a3aaece
NoahNim <nmedoff123@gmail.com>
2021-10-23 20:14:29 -0700

    made ad define form

--- front-end/podcast-hack/src/components/AdDefine/index.js
+++ front-end/podcast-hack/src/components/AdDefine/index.js
@@ -4,7 +4,18 @@ function AdDefine() {
 
     return (
         <div>
-            AD DEFINE COMPONENT
+            <form>
+                <label>Ad Name:</label>
+                <input type="text"></input>
+                <label>Ad File</label>
+                <input type="file" name="file"></input>
+                <label>First Ad TimeStamp</label>
+                <input type="time" name="timestamp" step="1"></input>
+                <label>Mid Ad TimeStamp</label>
+                <input type="time" name="timestamp" step="2"></input>
+                <label>Final Ad TimeStamp</label>
+                <input type="time" name="timestamp" step="3"></input>
+            </form>
         </div>
     )
 }
