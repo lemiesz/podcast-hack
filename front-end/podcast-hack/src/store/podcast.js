635097e937e2796ca29a904af151862d4c14c403
NoahNim <nmedoff123@gmail.com>
2021-10-30 16:49:14 -0700

    made action creators

--- front-end/podcast-hack/src/store/podcast.js
+++ front-end/podcast-hack/src/store/podcast.js
@@ -2,10 +2,27 @@
 const LOAD = "podcast/load";
 const ADD = "podcast/add";
 const EDIT = "podcast/edit";
-const DELETE = "podcast/delete";
+const REMOVE = "podcast/delete";
 
 // ACTION CREATORS
 
-const load = (podcast) => {
-    
-}
+const load = (podcast) => ({
+    type: LOAD,
+    podcast
+})
+
+const edit = (podcast) => ({
+    type: EDIT,
+    podcast
+})
+
+const add = (podcast) => ({
+    type: ADD,
+    podcast
+})
+
+const remove = (podcast) => ({
+    type: REMOVE,
+    podcast
+})
+
