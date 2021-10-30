5047c10fdd533f5aed4cc709c8a1c45aada4b83b
NoahNim <nmedoff123@gmail.com>
2021-10-30 16:46:24 -0700

    made podcast redux type constants

--- front-end/podcast-hack/src/store/index.js
+++ front-end/podcast-hack/src/store/index.js
@@ -9,14 +9,14 @@ let enhancer;
 
 // added code for when using a user environment such as a logged in user;
 
-// if (process.env.NODE_ENV === 'production') {
-//     enhancer = applyMiddleware(thunk);
-// } else {
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
     const logger = require('redux-logger').default;
     const composeEnhancers =
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
+}
 
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
---
+++ front-end/podcast-hack/src/store/podcast.js
@@ -0,0 +1,11 @@
+//TYPE CONSTANTS
+const LOAD = "podcast/load";
+const ADD = "podcast/add";
+const EDIT = "podcast/edit";
+const DELETE = "podcast/delete";
+
+// ACTION CREATORS
+
+const load = (podcast) => {
+    
+}
