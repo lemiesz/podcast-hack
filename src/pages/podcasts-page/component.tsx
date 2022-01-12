e5a19fb3be625a2d6a7c16dce102db2f80579768
♊️Elcid💰 <lemieszrobert@gmail.com>
2022-01-11 23:25:19 -0800

    Changing folder structure from nested /root/front-end/src -> /root/src
    Originall we thought of structuring this as a monorepo. With the front-end and back-end application being rooted at seperate folders.
    However it turned out to be more convienent to just root the backend function in the same directory where firebase was initialized (front-end).
    
    Additionally i found that our CI build in github actions was failing due to it expecting a `package.json` in the root location. Overall its easier to just restructure the folder layout like this.

--- src/pages/podcasts-page/component.tsx
+++ src/pages/podcasts-page/component.tsx
@@ -14,6 +14,70 @@ import { StoreState } from 'store'
 import podcastsSlice from 'store/podcast'
 import { LinkButton } from '../../components/buttons'
 
+export default function PodcastsPage() {
+    const user = useCurrentUser()
+    const podcasts = useSelector((state: StoreState) => state.podcasts)
+    const podcastArray = Object.values(podcasts)
+    const dispatch = useDispatch()
+    const hasPodcasts = podcastArray.length > 0
+
+    useEffect(() => {
+        api.getPodcasts(user.podcasts).then((result) => {
+            dispatch(
+                podcastsSlice.actions.setPodcasts(
+                    result.reduce((acc, curr) => {
+                        if (!curr) {
+                            return acc
+                        }
+                        return {
+                            ...acc,
+                            [curr.id]: curr,
+                        }
+                    }, {})
+                )
+            )
+        })
+    }, [dispatch, user])
+
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, ease: 'easeOut' }}
+            className="container flex flex-col p-8 justify-center m-auto"
+        >
+            <h1 className="text-3xl text-bold">Podcasts</h1>
+            <div className="flex flex-col md:flex-row gap-3 mt-7">
+                <div className="flex flex-col md:w-5/6 grid-4">
+                    {!hasPodcasts && <AddPodcast />}
+                    {hasPodcasts && <DisplayPodcasts />}
+                </div>
+                {hasPodcasts && <ActionsTable />}
+            </div>
+        </motion.div>
+    )
+}
+
+function ActionsTable() {
+    return (
+        <>
+            <LabelTable label="Actions" className="hidden md:flex max-h-52">
+                <div className="p-3">
+                    <LinkButton to={CreateRouteMap.createBase.path}>
+                        Create New Podcast
+                    </LinkButton>
+                </div>
+            </LabelTable>
+            <div className="mt-14 md:hidden">
+                <LinkButton to={CreateRouteMap.createBase.path}>
+                    Create New Podcast
+                </LinkButton>
+            </div>
+        </>
+    )
+}
+
 function AddPodcast() {
     return (
         <div className="flex-col justify-center mt-10 p-5 bg-white rounded-md border-2 border-gray-200 shadow-sm ">
@@ -70,60 +134,5 @@ function DisplayPodcasts() {
                 <PodcastCard key={podcast.id} podcast={podcast} />
             ))}
         </div>
-    )
-}
-export default function PodcastsPage() {
-    const user = useCurrentUser()
-    const podcasts = useSelector((state: StoreState) => state.podcasts)
-    const podcastArray = Object.values(podcasts)
-    const dispatch = useDispatch()
-    const hasPodcasts = podcastArray.length > 0
-
-    useEffect(() => {
-        api.getPodcasts(user.podcasts).then((result) => {
-            dispatch(
-                podcastsSlice.actions.setPodcasts(
-                    result.reduce((acc, curr) => {
-                        if (!curr) {
-                            return acc
-                        }
-                        return {
-                            ...acc,
-                            [curr.id]: curr,
-                        }
-                    }, {})
-                )
-            )
-        })
-    }, [dispatch, user])
-
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2, ease: 'easeOut' }}
-            className="container flex flex-col p-8 justify-center m-auto"
-        >
-            <h1 className="text-3xl text-bold">Podcasts</h1>
-            <div className="flex flex-col md:flex-row gap-3 mt-7">
-                <div className="flex flex-col md:w-5/6 grid-4">
-                    {!hasPodcasts && <AddPodcast />}
-                    {hasPodcasts && <DisplayPodcasts />}
-                </div>
-                <LabelTable label="Actions" className="hidden md:flex max-h-52">
-                    <div className="p-3">
-                        <LinkButton to={CreateRouteMap.createBase.path}>
-                            Create New Podcast
-                        </LinkButton>
-                    </div>
-                </LabelTable>
-                <div className="mt-14 md:hidden">
-                    <LinkButton to={CreateRouteMap.createBase.path}>
-                        Create New Podcast
-                    </LinkButton>
-                </div>
-            </div>
-        </motion.div>
     )
 }
